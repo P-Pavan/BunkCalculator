cfb9a4c0ed0125215ae855ed7cb69a4e5f49803d
P-Pavan <ppavan2002@gmail.com>
2022-08-20 23:40:05 +0530

    index-pages

--- index.js
+++ index.js
@@ -3,9 +3,12 @@ let submit = document.getElementById("submit");
 submit.onclick = function() {
     let taken = document.getElementById("taken").value;
     let attended = document.getElementById("attended").value;
-
-    let attendance = attended/taken * 100;
-
+    let bar = document.getElementById("bar");
+    let attendance = (attended/taken * 100).toFixed(2);
+    if(attendance>100) {
+        attendance = "kinda sus!!"
+    }else attendance += "%";
     document.getElementById("output").innerText = attendance;
+    bar.style.width = attendance;
 }
 
