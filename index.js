5cb33010f6d06cfd3a819dfa53a6f4fba22f1343
P-Pavan <ppavan2002@gmail.com>
2023-02-20 23:27:57 +0530

    backing up

--- index.js
+++ index.js
@@ -4,11 +4,18 @@ submit.onclick = function() {
     let taken = document.getElementById("taken").value;
     let attended = document.getElementById("attended").value;
     let bar = document.getElementById("bar");
+  //  let totalClasses = getElementById("toTake");
     let attendance = (attended/taken * 100).toFixed(2);
+    // let extraClasses = (0.75 * totalClasses) - totalAttended;
+
     if(attendance>100) {
         attendance = "kinda sus!!"
-    }else attendance += "%";
-    document.getElementById("output").innerText = attendance;
+    }else {
+        attendance += "%";
+        document.getElementById("bar").innerText = attendance;
+        // document.getElementById("output2").innerText = extraClasses;
+    }
+        document.getElementById("output").innerText = attendance;
     bar.style.width = attendance;
 }
 
